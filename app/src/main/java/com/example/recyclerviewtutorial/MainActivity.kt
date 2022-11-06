bd3c34ef8e337c510e3ab89917e6526344de948e
Vishma Pratim Das <bhishmadas9@gmail.com>
2022-11-06 22:55:09 +0530

    Initial stable version of scrolling list app

---
+++ app/src/main/java/com/example/recyclerviewtutorial/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.recyclerviewtutorial
+
+import MyCustomAdapter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Initialise data
+        var data = mutableListOf<Person>()
+
+        for (i in 1..100) {
+            if (i%3==0){
+                data.add(Person("Tricia",21))
+//                Log.i("Add data","name: ${data[i].name}")
+            } else if (i%2==0) {
+                data.add(Person("Jessica",24))
+//                Log.i("Add data","name: ${data[i].name}")
+            } else {
+                data.add(Person("Summer",22))
+//                Log.i("Add data","name: ${data[i].name}")
+            }
+        }
+
+        for (i in 1..data.lastIndex){
+            Log.i("Check data", "name: ${data[i].name}")
+        }
+
+
+        //Step 1: Add recyclerview to gradle config: DONE
+        //Step 2: Define data class : DONE
+        //Step 3: Add recyclerview to activity : DONE
+        //Step 4: Create custom row XML file to define what a row in recyclerview looks like: DONE
+        //Step 5: Create Recyclerview adapter and Viewholder
+        //Step 6: Bind the data to the view in the Viewholder in the
+        var rvApp=findViewById<RecyclerView>(R.id.rvContacts)
+        rvApp.adapter=MyCustomAdapter(this,data)
+        rvApp.layoutManager=LinearLayoutManager(this)
+    }
+
+    class Person(
+        var name: String,
+        var age: Int
+    )
+}
---
+++ app/src/main/java/com/example/recyclerviewtutorial/MyCustomAdapter.kt
@@ -0,0 +1,38 @@
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewtutorial.MainActivity
+import com.example.recyclerviewtutorial.R
+
+class MyCustomAdapter(val context: Context, val datalist: MutableList<MainActivity.Person>) : RecyclerView.Adapter<MyCustomAdapter.ViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // create a ViewHolder having a View object
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_view,parent,false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // Bind data to the Viewholder passed
+        val data=datalist[position]
+        holder.bind(data)
+    }
+
+    override fun getItemCount(): Int {
+        // Return array size
+        return datalist.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var name=itemView.findViewById<TextView>(R.id.tvName)
+        var age=itemView.findViewById<TextView>(R.id.tvAge)
+
+        fun bind(contact: MainActivity.Person) {
+            // Bind the data in contact to the views
+            name.text=contact.name
+            age.text= contact.age.toString()
+        }
+    }
+}
